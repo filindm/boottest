02ff0279bbb30a532631894287eeedbb411b34c8
Filin <dfilin@tcstemp.local>
2016-10-17 08:13:51 +0100

    Added more resource access methods

--- src/main/java/org/fdm/boot/HelloController.java
+++ src/main/java/org/fdm/boot/HelloController.java
@@ -1,9 +1,15 @@
 package org.fdm.boot;
 
+import java.util.List;
+
 // import javax.inject.Inject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 public class HelloController {
@@ -15,6 +21,16 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         return "Customer repo: " + customerRepository;
+    }
+
+    @RequestMapping(path="/customer/{id}", method=RequestMethod.GET)
+    public Customer getCustomer(@PathVariable Long id) {
+        return customerRepository.findOne(id);
+    }
+
+    @RequestMapping(path="/customersByLastName/{lastName}", method=RequestMethod.GET)
+    public List<Customer> getCustomersByLastName(@PathVariable String lastName) {
+        return customerRepository.findByLastName(lastName);
     }
 
 }
